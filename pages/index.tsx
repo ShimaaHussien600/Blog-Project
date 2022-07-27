372ffdbbcd6761745f6d82772ab42c3132c9e95e
shimaahussien600 <shimaahussien600@gmail.com>
2022-07-27 16:15:41 +0200

    config typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Layout/Header"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-3xl font-bold underline">
+        Welcome to my blog!
+      </h1>
+    </div>
+
+  )
+}
