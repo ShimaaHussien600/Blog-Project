e81cf06b07330ca126f8466ee58f8daf4aba50fc
shimaahussien600 <shimaahussien600@gmail.com>
2022-08-01 19:12:22 +0200

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,21 @@ import { AppProps } from 'next/app';
 import Layout from '../components/layout/index';
 import { store, wrapper } from '../app/store'
 import { Provider } from 'react-redux'
+import SEO from "@bradgarropy/next-seo"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Layout>
+        {/* a library that add multi meta  */}
+        <SEO title="مركز المدونة" description="An blog of Articles using Next.js and tailwindcss"
+          keywords={['nextjs', 'المدونة', 'مركز المدونة', 'Tailwind and Nextjs Blog']}
+          icon='/favicon.ico'
+          twitter={{
+            card: 'summary',
+            site: 'somesite',
+            image: '/vercel.svg',
+          }} />
         <Component {...pageProps} />
       </Layout>
     </Provider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
-       {/* to add public font family */}
+      <Head>
+        <title>Blog Home Page</title>
+        <meta name="description" content="An blog of Articles using Next.js and tailwindcss" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      </Head>
+      {/* to add public font family */}
       <body className='font-DINNext bg-gray-baby'>
         <Main />
         <NextScript />
